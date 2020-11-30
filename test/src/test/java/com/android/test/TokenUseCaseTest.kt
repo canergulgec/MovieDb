b0484af8115bc790ce313c1764e629b2efc7f4fa
Caner Gülgeç <canergulgec@gmail.com>
2020-11-30 22:39:23 +0300

    Add TokenViewModelTest Add LiveDataTestUtil

--- test/src/test/java/com/android/test/TokenUseCaseTest.kt
+++ test/src/test/java/com/android/test/TokenUseCaseTest.kt
@@ -1,38 +1,50 @@
 package com.android.test
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.android.base.Resource
 import com.android.data.model.remote.TokenResponse
 import com.android.domain.repository.NewTokenRepository
 import com.android.domain.usecase.NewTokenUseCase
-import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.stub
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class TokenUseCaseTest {
 
-    private val testDispatcher = TestCoroutineDispatcher()
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
-    private val newTokenRepository = mock<NewTokenRepository>()
-    private val newTokenUseCase = NewTokenUseCase(newTokenRepository, testDispatcher)
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
+
+    private val repository: NewTokenRepository = mock()
+
+    private lateinit var usecase: NewTokenUseCase
+
+    @Before
+    fun setup() {
+        usecase = NewTokenUseCase(repository, coroutineScope.dispatcher)
+    }
 
     @Test
     fun `new token flow emits successfully`() = runBlocking {
         val userDetails = TokenResponse(true, "1234567")
-        newTokenUseCase.stub {
-            onBlocking { execute() } doReturn flow {
-                emit(Resource.Loading)
-                emit(Resource.Success(userDetails))
-            }
+        val flow = flow {
+            emit(Resource.Loading)
+            emit(Resource.Success(userDetails))
         }
-        val flow = newTokenUseCase.execute()
-        flow.collectIndexed { index, value ->
+
+        whenever(usecase.execute()).thenReturn(flow)
+
+        val getNewToken = usecase.execute()
+        getNewToken.collectIndexed { index, value ->
             if (index == 0) assert(value == Resource.Loading)
             if (index == 1) assert(value is Resource.Success)
         }
---
+++ test/src/test/java/com/android/test/TokenViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.android.test
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.android.base.Resource
+import com.android.data.model.remote.TokenResponse
+import com.android.data.utils.DataStoreUtils
+import com.android.data.utils.SharedPreferencesUtils
+import com.android.domain.repository.NewTokenRepository
+import com.android.domain.usecase.NewTokenUseCase
+import com.android.presentation.vm.ProfileViewModel
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.verify
+
+@ExperimentalCoroutinesApi
+class TokenViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
+
+    private val newTokenRepository: NewTokenRepository = mock()
+
+    private val sharedPref: SharedPreferencesUtils = mock()
+
+    private val dataStoreUtils: DataStoreUtils = mock()
+
+    private lateinit var usecase: NewTokenUseCase
+
+    private lateinit var viewModel: ProfileViewModel
+
+    private val newSessionObserver: Observer<String> = mock()
+
+    @Before
+    fun setup() {
+        usecase = NewTokenUseCase(newTokenRepository, coroutineScope.dispatcher)
+        viewModel = ProfileViewModel(usecase, sharedPref, dataStoreUtils)
+    }
+
+    @Test
+    fun `new token flow emits successfully with argument captor`() = runBlocking {
+        val userDetails = TokenResponse(true, "1234567")
+        val flow = flow {
+            emit(Resource.Loading)
+            emit(Resource.Success(userDetails))
+        }
+
+        whenever(usecase.execute()).thenReturn(flow)
+        val captor = argumentCaptor<String>()
+        viewModel.newSessionLiveData.observeForever(newSessionObserver)
+
+        viewModel.getNewToken()
+
+        verify(newSessionObserver, times(1)).onChanged(captor.capture())
+        assertEquals(userDetails.requestToken, captor.firstValue)
+    }
+
+    @Test
+    fun `new token flow emits successfully with liveData util`() = runBlocking {
+        val userDetails = TokenResponse(true, "1234567")
+        val flow = flow {
+            emit(Resource.Loading)
+            emit(Resource.Success(userDetails))
+        }
+
+        whenever(usecase.execute()).thenReturn(flow)
+        viewModel.getNewToken()
+
+        assertEquals(userDetails.requestToken, viewModel.newSessionLiveData.getOrAwaitValue())
+    }
+}
