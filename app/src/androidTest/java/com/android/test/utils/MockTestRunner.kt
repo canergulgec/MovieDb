c70be063eb0d872fcc4138336257685fef8fcd0b
Caner Gülgeç <canergulgec@gmail.com>
2021-01-24 17:35:41 +0300

    Update MockTestRunner

--- app/src/androidTest/java/com/android/test/utils/MockTestRunner.kt
+++ app/src/androidTest/java/com/android/test/utils/MockTestRunner.kt
@@ -2,18 +2,10 @@ package com.android.test.utils
 
 import android.app.Application
 import android.content.Context
-import android.os.Bundle
-import android.os.StrictMode
 import androidx.test.runner.AndroidJUnitRunner
 import com.android.test.MovieTestApp
 
 class MockTestRunner : AndroidJUnitRunner() {
-
-    override fun onCreate(arguments: Bundle?) {
-        super.onCreate(arguments)
-
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
-    }
 
     override fun newApplication(cl: ClassLoader?, className: String?,
                                 context: Context?): Application {
