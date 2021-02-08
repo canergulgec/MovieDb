b8d7b084d6fa43c2aae2fdf725d55ab13d763743
Caner Gülgeç <canergulgec@gmail.com>
2021-02-08 21:45:23 +0300

    Add navController destinationChangedListener in MainActivity

--- app/src/main/java/com/android/moviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/android/moviedb/ui/main/MainActivity.kt
@@ -2,11 +2,13 @@ package com.android.moviedb.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.android.base.BaseActivity
 import com.android.moviedb.R
 import com.android.moviedb.databinding.ActivityMainBinding
+import com.caner.common.ext.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,12 +16,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val navController by lazy { findNavController(R.id.mainNavHostFragment) }
 
+    private val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+        when (destination.id) {
+            R.id.movieDetailFragment -> binding.bottomNavigation.visible(false)
+            else -> binding.bottomNavigation.visible(true)
+        }
+    }
+
     override fun initView(savedInstanceState: Bundle?) {
         setupViews()
     }
 
     private fun setupViews() {
         binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(listener)
     }
 
     override val bindLayout: (LayoutInflater) -> ActivityMainBinding
